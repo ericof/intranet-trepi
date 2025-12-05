a7c2ebf956e6ddb8d866e42a6b819614d012093e
Érico Andrei <ericof@gmail.com>
2025-12-05 12:02:52 -0300

    Exercício 48: Adiciona testes para o endpoint /@clima

--- backend/src/trepi/intranet/services/clima/openmeteo.py
+++ backend/src/trepi/intranet/services/clima/openmeteo.py
@@ -8,7 +8,7 @@ import requests
 BASE_URL: str = "https://api.open-meteo.com/v1/forecast"
 
 
-def time_30m_key(*args, **kwargs) -> float:
+def time_30m_key(*args, **kwargs):
     """Chave de cache para manter o valor em cache por 30 minutos."""
     return time() // (60 * 30)
 
@@ -67,8 +67,19 @@ def _formatar_resposta(data: dict) -> dict:
     }
 
 
-def _dados_clima(latitude: str, longitude: str, timezone: str) -> dict:
-    """Função que chama o serviço Open Meteo e retorna os dados de clima."""
+def _obtem_dados_open_meteo(params: dict[str, str]) -> dict:
+    """Realiza chamada ao serviço Open Meteo.
+
+    Esse método existe para facilitar o mock em testes.
+    """
+    # Parametros da chamada
+    response = requests.get(BASE_URL, params=params, timeout=5)
+    return response.json()
+
+
+@cache(time_30m_key)
+def dados_clima(latitude: str, longitude: str, timezone: str) -> dict:
+    """Chama o serviço Open Meteo e retorna os dados de clima."""
     # Parametros da chamada
     params = {
         "latitude": latitude,
@@ -81,13 +92,7 @@ def _dados_clima(latitude: str, longitude: str, timezone: str) -> dict:
     }
     # Realiza a requisição
     logger.info("Acesso ao OpenMeteo")
-    response = requests.get(BASE_URL, params=params, timeout=5)
+    raw_data = _obtem_dados_open_meteo(params)
     logger.info("Parseia dados recebidos")
-    data = _formatar_resposta(response.json())
+    data = _formatar_resposta(raw_data)
     return data
-
-
-@cache(time_30m_key)
-def dados_clima(latitude: str, longitude: str, timezone: str) -> dict:
-    """Chama o serviço Open Meteo e retorna os dados de clima."""
-    return _dados_clima(latitude, longitude, timezone)
